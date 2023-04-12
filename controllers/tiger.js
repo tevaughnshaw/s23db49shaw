e58535102bb77522a4e77b54a45b9ab6b5c9e8cc
tevaughnshaw <tevaughn.shaw1@gmail.com>
2023-04-12 18:16:21 -0500

    finished assignment POST req

--- controllers/tiger.js
+++ controllers/tiger.js
@@ -15,9 +15,25 @@ exports.tiger_detail = function(req, res) {
  res.send('NOT IMPLEMENTED: Tiger detail: ' + req.params.id);
 };
 // Handle Costume create on POST.
-exports.tiger_create_post = function(req, res) {
- res.send('NOT IMPLEMENTED: Tiger create POST');
-};
+exports.tiger_create_post = async function(req, res) {
+    console.log(req.body)
+    let document = new Tiger();
+    // We are looking for a body, since POST does not have query parameters.
+    // Even though bodies can be in many different formats, we will be picky
+    // and require that it be a json object
+    // {"costume_type":"goat", "cost":12, "size":"large"}
+    document.tail = req.body.tail;
+    document.color = req.body.color;
+    document.weight = req.body.weight;
+    try{
+    let result = await document.save();
+    res.send(result);
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
 // Handle Costume delete form on DELETE.
 exports.tiger_delete = function(req, res) {
  res.send('NOT IMPLEMENTED: Tiger delete DELETE ' + req.params.id);
@@ -26,3 +42,15 @@ exports.tiger_delete = function(req, res) {
 exports.tiger_update_put = function(req, res) {
  res.send('NOT IMPLEMENTED: Tiger update PUT' + req.params.id);
 };
+// VIEWS
+// Handle a show all view
+exports.tiger_view_all_Page = async function(req, res) {
+    try{
+    theTigers = await Tiger.find();
+    res.render('tiger', { title: 'Tiger Search Results', results: theTigers });
+    }
+    catch(err){
+    res.status(500);
+    res.send(`{"error": ${err}}`);
+    }
+   };
--- routes/tiger.js
+++ routes/tiger.js
@@ -1,9 +1,8 @@
 var express = require('express');
+const tiger_controlers= require('../controllers/tiger');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('tiger', { title: 'Search Results Tiger' });
-});
+router.get('/', tiger_controlers.tiger_view_all_Page );
 
 module.exports = router;
