c1dbdbd65c8eca31ccbae4c9a9774e8111561d11
tevaughnshaw <tevaughn.shaw1@gmail.com>
2023-04-27 10:51:23 -0500

    completed view buttons for collections

--- models/tiger.js
+++ models/tiger.js
@@ -2,7 +2,11 @@ const mongoose = require("mongoose")
 const tigerSchema = mongoose.Schema({
 tail: String,
 color: String,
-weight: Number
+weight: {
+   type: Number,
+   min: [100, 'weight must be more than 100 pounds'],
+   max: 20000
+}
 })
 module.exports = mongoose.model("Tiger",
 tigerSchema)
--- routes/tiger.js
+++ routes/tiger.js
@@ -2,6 +2,16 @@ var express = require('express');
 const tiger_controlers= require('../controllers/tiger');
 var router = express.Router();
 
+// A little function to check if we have an authorized user and continue on or
+// redirect to login.
+const secured = (req, res, next) => {
+ if (req.user){
+ return next();
+ }
+ req.session.returnTo = req.originalUrl;
+ res.redirect("/login");
+ }
+
 /* GET home page. */
 router.get('/', tiger_controlers.tiger_view_all_Page );
 
@@ -13,7 +23,7 @@ router.get('/detail', tiger_controlers.tiger_view_one_Page);
 /* GET create tiger page */
 router.get('/create', tiger_controlers.tiger_create_Page);
 /* GET create update page */
-router.get('/update', tiger_controlers.tiger_update_Page);
+router.get('/update',secured, tiger_controlers.tiger_update_Page);
 /* GET delete tiger page */
 router.get('/delete', tiger_controlers.tiger_delete_Page);
 
